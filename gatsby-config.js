b0e648ea6b625249668d459107958e9ac8e2784e
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2021-08-01 20:15:00 +0530

    removed netlify deploy config from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,6 @@
-const {
-  NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://techstack.lk',
-  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-  CONTEXT: NETLIFY_ENV = NODE_ENV,
-} = process.env;
-const isNetlifyProduction = NETLIFY_ENV === 'production';
-const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
-
 module.exports = {
   siteMetadata: {
-    siteUrl,
+    siteUrl: 'https://techstack.lk',
     title: 'Tech Stack LK',
   },
   plugins: [
@@ -48,24 +39,6 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        resolveEnv: () => NETLIFY_ENV,
-        env: {
-          production: {
-            policy: [{ userAgent: '*' }],
-          },
-          'branch-deploy': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-          'deploy-preview': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-        },
-      },
     },
   ],
 };
