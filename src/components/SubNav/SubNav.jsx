79a4b340705549029fb5f726784396e90c656473
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2021-08-31 10:53:04 +0530

    fixed eslint issue in sub nav

--- src/components/SubNav/SubNav.jsx
+++ src/components/SubNav/SubNav.jsx
@@ -23,21 +23,19 @@ export default function SubNav() {
   return (
     <div className="hidden items-center pb-1 mx-4 h-10 tracking-wider text-gray-500 md:flex md:justify-between lg:h-12 lg:text-xl xl:tracking-widest 2xl:pb-4 2xl:mt-4 2xl:text-3xl">
       <div className="flex items-center h-full md:hidden xl:flex">
-        {productCategories.map((category) => {
-          return (
-            <Link
-              className={
-                category.name === productCategories[0].name
-                  ? 'pl-1 pr-2 py-2 lg:py-4'
-                  : 'px-4 py-2 lg:py-4'
-              }
-              to={category.href}
-              key={category.name}
-            >
-              {category.name}
-            </Link>
-          );
-        })}
+        {productCategories.map((category) => (
+          <Link
+            className={
+              category.name === productCategories[0].name
+                ? 'pl-1 pr-2 py-2 lg:py-4'
+                : 'px-4 py-2 lg:py-4'
+            }
+            to={category.href}
+            key={category.name}
+          >
+            {category.name}
+          </Link>
+        ))}
       </div>
       <div className="flex items-center w-52 h-full">
         <Link to="/why-us" className="py-2 px-4" activeClassName=" active-link">
