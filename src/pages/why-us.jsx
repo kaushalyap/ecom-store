caed21e1293e4ff0a1bc1d18a38e00832c459b8d
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2021-10-18 10:52:55 +0530

    added light social icons, added why-us page.

---
+++ src/pages/why-us.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function WhyUs() {
+  return (
+    <Layout>
+      <h1>Why Us?</h1>
+    </Layout>
+  );
+}
