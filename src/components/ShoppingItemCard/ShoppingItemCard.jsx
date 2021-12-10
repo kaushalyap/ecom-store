2970cc8d2fe978c259f3c9761f9358968dafd2bb
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2021-12-10 12:53:29 +0530

    added editable prop to ShoppingItemCard, finished order summary layout

--- src/components/ShoppingItemCard/ShoppingItemCard.jsx
+++ src/components/ShoppingItemCard/ShoppingItemCard.jsx
@@ -10,7 +10,7 @@ const itemCount = [
   { id: 3, count: '3' },
 ];
 
-export default function ShoppingItemCard({ title, price }) {
+export default function ShoppingItemCard({ title, price, editable }) {
   return (
     <div className="flex gap-4 mb-4 bg-green-50 rounded-lg shadow-lg md:mb-6 md:h-44">
       <img
@@ -23,16 +23,26 @@ export default function ShoppingItemCard({ title, price }) {
           <h3 className="mb-2 font-headline md:text-xl">{trimText(title)}</h3>
         </Link>
         <div className="flex justify-between items-end mb-2">
-          <ListBox options={itemCount} />
-          <span className="text-lg tracking-wide md:text-xl">
-            {`Rs. ${price}`}
-          </span>
+          {editable ? (
+            <ListBox options={itemCount} />
+          ) : (
+            <span className="tracking-wide text-gray-500">Qty 1</span>
+          )}
+
+          <span className=" tracking-wide md:text-xl">{`Rs. ${price}`}</span>
         </div>
-        <div className="flex justify-end text-gray-400">
-          <button type="button" className="text-sm tracking-wide md:text-base">
-            Remove
-          </button>
-        </div>
+        {editable ? (
+          <div className="flex justify-end text-gray-400">
+            <button
+              type="button"
+              className="text-sm tracking-wide md:text-base"
+            >
+              Remove
+            </button>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   );
@@ -41,4 +51,5 @@ export default function ShoppingItemCard({ title, price }) {
 ShoppingItemCard.propTypes = {
   price: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  editable: PropTypes.bool.isRequired,
 };
--- src/pages/order-summary.jsx
+++ src/pages/order-summary.jsx
@@ -1,5 +1,86 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO/SEO';
+import ShoppingItemCard from '../components/ShoppingItemCard/ShoppingItemCard';
 
 export default function OrderSummary() {
-  return <div />;
+  return (
+    <Layout>
+      <SEO title="Checkout" />
+      <main className="mx-5 mb-20 lg:mx-auto lg:w-2/3 xl:container xl:mx-auto xl:w-full">
+        <h1 className="mt-10 mb-5 font-headline text-xl font-medium md:mb-8 md:text-2xl xl:text-3xl">
+          Thank you for choosing us!
+        </h1>
+        <div className="xl:flex xl:gap-16">
+          <div className="xl:w-3/5">
+            <p className="p-3 mb-4 bg-green-100 rounded-xl shadow-lg md:px-4 md:mb-6 md:tracking-wide">
+              Payment Successful, order placed!
+            </p>
+            <div className="mb-6">
+              <h2 className="mb-2 font-headline text-lg tracking-wide md:text-xl">
+                Your Cart
+              </h2>
+
+              <ShoppingItemCard
+                title="Oculus - Quest 2 Advanced All-In-One Virtual Reality Headset - 128GB"
+                price={5800}
+                editable={false}
+              />
+              <ShoppingItemCard
+                title="Oculus - Quest 2 Advanced All-In-One Virtual Reality Headset - 128GB"
+                price={5800}
+                editable={false}
+              />
+
+              <div className="p-4 bg-blue-100 rounded-xl shadow-lg">
+                <h3 className="mb-2 font-headline tracking-wide md:text-lg">
+                  Summary
+                </h3>
+
+                <p className=" flex justify-between tracking-wide text-gray-500 md:text-lg">
+                  <span>Sub Total</span>
+                  <span>Rs. 11,200</span>
+                </p>
+                <p className="flex justify-between tracking-wide text-gray-500 md:text-lg">
+                  <span>Delivery</span>
+                  <span>FREE</span>
+                </p>
+                <p className="flex justify-between mb-2 tracking-wide text-gray-500 md:text-lg">
+                  <span>Discount</span>
+                  <span>0%</span>
+                </p>
+                <p className="flex justify-between font-bold tracking-wide md:text-lg xl:tracking-wider">
+                  <span>Total</span>
+                  <span>Rs. 11,200</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="xl:w-1/5">
+            <div className="py-2 mb-6 rounded-lg">
+              <h2 className="mb-2 font-headline text-lg tracking-wide md:text-xl xl:mt-16">
+                Customer Info
+              </h2>
+              <div className="p-4 tracking-wide text-gray-600 bg-blue-100 rounded-xl shadow-lg md:text-lg">
+                <p>John Doe</p>
+                <p>0731 1231 12313</p>
+                <p>john.doe@gmail.com</p>
+              </div>
+            </div>
+            <div className="mb-6">
+              <h2 className="mb-2 font-headline text-lg tracking-wide md:text-xl">
+                Delivery Address
+              </h2>
+              <div className="p-4 tracking-wide text-gray-600 bg-blue-100 rounded-xl shadow-lg md:text-lg">
+                <p>Address line one here</p>
+                <p>Nugegoda,</p>
+                <p>Colombo,</p>
+                <p>10250</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
 }
